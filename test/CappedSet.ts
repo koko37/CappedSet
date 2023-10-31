4f79c75d65050ba134867399433fda66f16e0bd7
koko <engel0088@gmail.com>
2023-10-31 11:40:06 -0400

    add test

--- test/CappedSet.ts
+++ test/CappedSet.ts
@@ -6,20 +6,19 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-const CAP_SIZE = 100;
+const CAP_SIZE = 4;
 
 describe("CappedSet", function () {
   async function deployCappedSetFixture() {
-    const [user, addr1, addr2, addr3] = await ethers.getSigners();
     const CappedSet = await ethers.getContractFactory("CappedSet");
     const contract = await CappedSet.deploy(CAP_SIZE);
 
-    // it has no way to read return value of no-view/no-pure function from off-chain
-    // so it uses another contract that emits the result as event
+    // it has no way to read return value of non-view/non-pure function from off-chain
+    // so it uses wrapper contract that emits the result as event
     const TestCappedSet = await ethers.getContractFactory("TestCappedSet");
     const wrapperContract = await TestCappedSet.deploy(contract.target);
 
-    return { user, addr1, addr2, addr3, contract, wrapperContract };
+    return { contract, wrapperContract };
   }
   describe("Deployment", function () {
     it("Should reject if cap size is invalid", async function () {
@@ -38,7 +37,8 @@ describe("CappedSet", function () {
 
   describe("Insert", function () {
     it("Should reject if address or value is invalid", async function () {
-      const { contract, addr1 } = await loadFixture(deployCappedSetFixture);
+      const { contract } = await loadFixture(deployCappedSetFixture);
+      const [addr1] = await ethers.getSigners();
 
       await expect(contract.insert(ethers.ZeroAddress, 0)).to.be.revertedWith(
         "invalid element"
@@ -49,17 +49,77 @@ describe("CappedSet", function () {
       await expect(contract.insert(addr1.address, 0)).to.be.revertedWith(
         "invalid element"
       );
+      await expect(contract.insert(addr1.address, 1)).not.be.reverted;
     });
 
     it("Should return zero after adding 1st element", async function () {
-      const { wrapperContract, addr1 } = await loadFixture(
-        deployCappedSetFixture
-      );
+      const { wrapperContract } = await loadFixture(deployCappedSetFixture);
+      const [addr1] = await ethers.getSigners();
 
       // const tx = await contract.insert(addr1, 1);
       await expect(wrapperContract.insert(addr1, 1))
         .to.emit(wrapperContract, "Inserted")
         .withArgs(ethers.ZeroAddress, 0);
+    });
+
+    it("Should reject duplicate address or value", async function () {
+      const { contract } = await loadFixture(deployCappedSetFixture);
+      const [addr1, addr2] = await ethers.getSigners();
+
+      await contract.insert(addr1.address, 1);
+      // try to insert with same address
+      await expect(contract.insert(addr1.address, 2)).to.be.revertedWith(
+        "already exists"
+      );
+      // try to insert with same value
+      await expect(contract.insert(addr2.address, 1)).to.be.revertedWith(
+        "already exists"
+      );
+      await expect(contract.insert(addr2.address, 2)).not.be.reverted;
+    });
+
+    it("Should return lowest element after adding 2nd element", async function () {
+      const { contract, wrapperContract } = await loadFixture(
+        deployCappedSetFixture
+      );
+      const [addr1, addr2, addr3] = await ethers.getSigners();
+
+      // adding 1st element, return 0
+      await contract.insert(addr1.address, 3);
+      // adding 2nd element
+      await expect(wrapperContract.insert(addr2.address, 2))
+        .to.emit(wrapperContract, "Inserted")
+        .withArgs(addr2.address, 2);
+      expect(await contract.numElements()).to.equal(2);
+
+      // adding 3rd element
+      await expect(wrapperContract.insert(addr3.address, 4))
+        .to.emit(wrapperContract, "Inserted")
+        .withArgs(addr2.address, 2);
+      expect(await contract.numElements()).to.equal(3);
+    });
+
+    it("Should boot out lowest element if cap is reached already", async function () {
+      const { contract, wrapperContract } = await loadFixture(
+        deployCappedSetFixture
+      );
+      const addrs = await ethers.getSigners();
+
+      await contract.insert(addrs[0].address, 3);
+      await contract.insert(addrs[1].address, 4);
+      await contract.insert(addrs[2].address, 2);
+      await expect(wrapperContract.insert(addrs[3].address, 5))
+        .to.emit(wrapperContract, "Inserted")
+        .withArgs(addrs[2].address, 2);
+
+      expect(await contract.numElements()).to.equal(4);
+
+      // cap is reached
+      await expect(wrapperContract.insert(addrs[4].address, 6))
+        .to.emit(wrapperContract, "Inserted")
+        .withArgs(addrs[0].address, 3);
+
+      expect(await contract.numElements()).to.equal(4);
     });
   });
 
