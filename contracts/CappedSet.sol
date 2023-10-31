ef95e517dca7f6ed0ab4a086e85b30f30490fb43
koko <engel0088@gmail.com>
2023-10-31 08:38:16 -0400

    add insert/remove feature

--- contracts/CappedSet.sol
+++ contracts/CappedSet.sol
@@ -6,21 +6,58 @@ import "./BokkyPooBahsRedBlackTreeLibrary.sol";
 contract CappedSet {
     using BokkyPooBahsRedBlackTreeLibrary for BokkyPooBahsRedBlackTreeLibrary.Tree;
 
+    uint256 public immutable cap;
+
     BokkyPooBahsRedBlackTreeLibrary.Tree valueTree;
+    mapping(address => uint256) values;
+    mapping(uint256 => address) addresses;
+
     uint256 public numElements;
 
-    constructor(uint256 _numElements) {
-        numElements = _numElements;
+    constructor(uint256 _cap) {
+        cap = _cap;
     }
 
     function insert(
         address addr,
         uint256 value
-    ) public returns (address newLowestAddress, uint256 newLowestValue) {}
+    ) public returns (address newLowestAddress, uint256 newLowestValue) {
+        require(addr != address(0) && value != 0, "invalid element");
+        require(!valueTree.exists(value), "already exists");
+
+        uint256 lowestValue = valueTree.first();
+        require(value > lowestValue || numElements < cap, "too low value");
+
+        if (numElements == cap) {
+            // it has reached to cap already, so removes current lowest value.
+            _removeElement(lowestValue);
+
+            // get new lowest value
+            lowestValue = valueTree.first();
+        } else {
+            numElements++;
+        }
+
+        valueTree.insert(value);
+        values[addr] = value;
+        addresses[value] = addr;
+
+        newLowestAddress = value > lowestValue ? addresses[lowestValue] : addr;
+        newLowestValue = value > lowestValue ? lowestValue : value;
+    }
 
     function remove(
         address addr
-    ) public returns (address newLowestAddress, uint256 newLowestValue) {}
+    ) public returns (address newLowestAddress, uint256 newLowestValue) {
+        require(addr != address(0), "invalid element");
+
+        uint256 value = values[addr];
+        require(value != 0, "invalid element");
+
+        _removeElement(value);
+        newLowestValue = valueTree.first();
+        newLowestAddress = addresses[newLowestValue];
+    }
 
     function update(
         address addr,
@@ -28,4 +65,12 @@ contract CappedSet {
     ) public returns (address newLowestAddress, uint256 newLowestValue) {}
 
     function getValue(address addr) public view returns (uint256) {}
+
+    function _removeElement(uint256 value) internal {
+        address _address = addresses[value];
+
+        valueTree.remove(value);
+        delete values[_address];
+        delete addresses[value];
+    }
 }
