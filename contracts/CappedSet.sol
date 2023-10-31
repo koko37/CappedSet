c59ffe38ed676140e45964e194289c87692a3471
koko <engel0088@gmail.com>
2023-10-31 05:54:19 -0400

    initial commit

---
+++ contracts/CappedSet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CappedSet {
+    uint256 public numElements;
+
+    constructor(uint256 _numElements) {
+        numElements = _numElements;
+    }
+
+    function insert(
+        address addr,
+        uint256 value
+    ) public returns (address newLowestAddress, uint256 newLowestValue) {}
+
+    function remove(
+        address addr
+    ) public returns (address newLowestAddress, uint256 newLowestValue) {}
+
+    function update(
+        address addr,
+        uint256 newVal
+    ) public returns (address newLowestAddress, uint256 newLowestValue) {}
+
+    function getValue(address addr) public view returns (uint256) {}
+}
